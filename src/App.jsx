23c88ab4328e5628fa4e0ea5ff6e53efdad73621
Farfama <farfama.hargaaya@gmail.com>
2023-04-07 01:13:56 +0900

    Add Palindrome challenge
    -

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PalindromeChallenge from "./component/PalindromeChallenge";
+
+function App() {
+  return (
+    <div className="App">
+      <PalindromeChallenge />
+    </div>
+  );
+}
+
+export default App;
