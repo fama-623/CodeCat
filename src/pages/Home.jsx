363a46403fb351519c9bb271a1bb0ef9fd1b3b83
Farfama <farfama.hargaaya@gmail.com>
2023-04-07 02:07:37 +0900

    Add Tailwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,20 +20,23 @@ function Home() {
   };
 
   return (
-    <div className="home">
-      <h1>Welcome to the Coding Challenges</h1>
-      <div className="challenge-buttons">
-        <button onClick={() => handleChallengeClick(SumChallenge)}>
-          Sum of Two Numbers
-        </button>
-        <button onClick={() => handleChallengeClick(PalindromeChallenge)}>
-          Palindrome Checker
-        </button>
-        <button onClick={() => handleChallengeClick(AcronymChallenge)}>
-          Acronym Generator
-        </button>
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-10">Welcome to the Coding Challenges</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="border rounded p-6 bg-white">
+          <h2 className="font-bold mb-4">Sum of Two Numbers</h2>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleChallengeClick(SumChallenge)}>Start Challenge</button>
+        </div>
+        <div className="border rounded p-6 bg-white">
+          <h2 className="font-bold mb-4">Palindrome Checker</h2>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleChallengeClick(PalindromeChallenge)}>Start Challenge</button>
+        </div>
+        <div className="border rounded p-6 bg-white">
+          <h2 className="font-bold mb-4">Acronym Generator</h2>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleChallengeClick(AcronymChallenge)}>Start Challenge</button>
+        </div>
       </div>
-      <div className="current-challenge">{renderCurrentChallenge()}</div>
+      <div className="mt-10">{renderCurrentChallenge()}</div>
     </div>
   );
 }
